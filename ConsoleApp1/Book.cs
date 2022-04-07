86ab7b364d66406ef2f9fb2a27688ad8d55a90cb
ibrahimhuseynov <tu7pdt8l8@code.edu.az>
2022-04-07 11:48:25 +0400

    hjdsgkdfad

---
+++ ConsoleApp1/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Book
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }    
+        public string AuthorName { get; set; }  
+        public double Price { get; set; }   
+        public string ShowInfo()
+        {
+           return  $"Id - {Id}  Name -{Name}  AuthorName - {AuthorName}  Price - {Price} ";
+        }
+
+
+    }
+}
---
+++ ConsoleApp1/Library.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Library
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; } 
+        public List<Book> Books { get; set; } = new List<Book>();
+        public void AddBook(Book book1)
+        {
+            Books.Add(book1);
+        }
+        public List<Book> GetBookById(int? _Id)
+        {
+            return Books.FindAll(x => x.Id == _Id);
+        }
+        public void  RemoveBook(int _Id)
+        {
+            var resalt=Books.FindAll(x => x.Id == _Id);
+            Books.Remove(resalt);
+        }
+    }
+}
